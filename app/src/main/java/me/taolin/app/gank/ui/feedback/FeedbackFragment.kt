ab7f59acac0ba5e97e79698bfa35b7f57c865c63
Taolin <taolin@cvte.com>
2018-03-22 15:06:11 +0800

    small fix.

--- app/src/main/java/me/taolin/app/gank/ui/feedback/FeedbackFragment.kt
+++ app/src/main/java/me/taolin/app/gank/ui/feedback/FeedbackFragment.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -53,8 +55,19 @@ class FeedbackFragment : BaseFragment() {
         feedbackList.layoutManager = LinearLayoutManager(context)
 
         sendBtn.setOnClickListener {
-            sendFeedback(feedbackEditor.text.toString())
+            sendFeedback(feedbackEditor.text.trim().toString())
+            feedbackEditor.setText("")
         }
+
+        feedbackEditor.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                sendBtn.isEnabled = feedbackEditor.text.toString().isNotEmpty()
+            }
+        })
 
         setFeedbackUserName()
         feedbackThread.sync(feedbackCallback)
